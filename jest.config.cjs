acfa663fdcb6f8cd33f8548e612d150b220c4e89
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2026-04-08 08:25:12 +0200

    chore: configure Jest for ESM and add first unit test

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,18 @@
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
